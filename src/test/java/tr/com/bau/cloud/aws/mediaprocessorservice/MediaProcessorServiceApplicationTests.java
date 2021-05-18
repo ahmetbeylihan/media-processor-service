480d5f212cf7b01172dd49702e170dc6f0484f65
ahmet <ahmet.beylihan@gmail.com>
2021-05-19 01:31:04 +0300

    Initial commit

---
+++ src/test/java/tr/com/bau/cloud/aws/mediaprocessorservice/MediaProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.bau.cloud.aws.mediaprocessorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaProcessorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
